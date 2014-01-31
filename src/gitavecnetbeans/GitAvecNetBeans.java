09e222ab379ebfda59089bc5e7c505f1e12e6477
Hugo Scurti <hugoscurti@gmail.com>
2014-01-30 23:23:40 -0300

    Ajout d'une fonction afficherMessage Appel de la fonction afficherMessage dans le main Ajout du fichier collection.json

--- src/gitavecnetbeans/GitAvecNetBeans.java
+++ src/gitavecnetbeans/GitAvecNetBeans.java
@@ -1,22 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package gitavecnetbeans;
 
-/**
- *
- * @author ch891311
- */
 public class GitAvecNetBeans {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Voici un projet qui est configuré avec Git");
+        GitAvecNetBeans.afficherMessage();
+
     }
     
+    public static void afficherMessage() {
+        System.out.println("Ceci est un message");
+    }
+  
 }
