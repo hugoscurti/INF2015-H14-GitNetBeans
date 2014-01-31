42f15af85b079abcd6114104a159ff97e27a715a
Hugo Scurti <hugoscurti@gmail.com>
2014-01-30 23:49:53 -0300

    Ajout de la méthode afficherDateAujourdhui

--- src/gitavecnetbeans/GitAvecNetBeans.java
+++ src/gitavecnetbeans/GitAvecNetBeans.java
@@ -1,16 +1,23 @@
 
 package gitavecnetbeans;
 
+import java.util.Date;
+
 public class GitAvecNetBeans {
 
     public static void main(String[] args) {
         System.out.println("Voici un projet qui est configuré avec Git");
-        GitAvecNetBeans.afficherMessage();
+        afficherMessage();
+        afficherDateAjourdhui();
 
     }
     
     public static void afficherMessage() {
         System.out.println("Bonjour");
+    }
+    
+    public static void afficherDateAjourdhui(){
+        System.out.println(new Date().toString());
     }
   
 }
